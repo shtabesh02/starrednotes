e05c58203d72e04739b29117bb547565df81fba1
shtabesh02 <shtabesh02@gmail.com>
2024-05-02 17:45:39 -0400

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,11 +7,26 @@ const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+
+
+router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
   
+
+
+
+
 
   // GET /api/set-token-cookie
 router.get('/set-token-cookie', async (_req, res) => {
