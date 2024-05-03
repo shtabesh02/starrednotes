60fc597814b33f7b0e15e7b91654ae632ec38ea6
shtabesh02 <shtabesh02@gmail.com>
2024-05-02 20:07:05 -0400

    first and last name added. also package json file created at the root of project.

--- backend/db/seeders/20240502204411-demo-user.js
+++ backend/db/seeders/20240502204411-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'Sharif',
+        lastName: 'Rezaie',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Alisina',
+        lastName: 'Danesh',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Mohammadjan',
+        lastName: 'Farasu',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -51,6 +51,8 @@ router.post(
   
       const safeUser = {
         id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
         email: user.email,
         username: user.username,
       };
@@ -82,6 +84,8 @@ router.get(
       if (user) {
         const safeUser = {
           id: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
           email: user.email,
           username: user.username,
         };
