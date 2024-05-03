44a2c328e1ca338cac88abd65cb527c38977373e
shtabesh02 <shtabesh02@gmail.com>
2024-05-03 01:13:57 -0400

    signup form

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -45,8 +45,29 @@ const sessionReducer = (state = initialState, action) => {
   }
 };
 
+
+
 export const restoreUser = () => async (dispatch) => {
     const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+
+
+  export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password
+      })
+    });
     const data = await response.json();
     dispatch(setUser(data.user));
     return response;
