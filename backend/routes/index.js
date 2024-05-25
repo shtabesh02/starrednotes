89fede341764978647706a04ca65778c1adc5278
shtabesh02 <shtabesh02@gmail.com>
2024-05-25 12:46:26 -0400

    build to dist in route index

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     return res.sendFile(
-      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
     );
   });
 }
