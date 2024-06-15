1d85ab815905f12fc5b5a878d492baa0f75ee944
shtabesh02 <shtabesh02@gmail.com>
2024-06-14 20:47:57 -0400

    options for migration

--- backend/db/migrations/20240602185842-create-starred-note.js
+++ backend/db/migrations/20240602185842-create-starred-note.js
@@ -1,5 +1,10 @@
 'use strict';
-/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('StarredNotes', {
@@ -32,9 +37,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
+    options.tableName = "StarredNotes";
     await queryInterface.dropTable('StarredNotes');
   }
 };
--- backend/db/migrations/20240613033842-create-completedlesson.js
+++ backend/db/migrations/20240613033842-create-completedlesson.js
@@ -1,5 +1,10 @@
 'use strict';
-/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Completedlessons', {
@@ -26,9 +31,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
+    options.tableName = "Completedlessons";
     await queryInterface.dropTable('Completedlessons');
   }
 };
