e95e164c4615b00a15a698b8a14ba5583b5dcf07
shtabesh02 <shtabesh02@gmail.com>
2024-06-14 20:51:30 -0400

    notes

--- backend/db/migrations/20240602190712-create-starred-note-comment.js
+++ backend/db/migrations/20240602190712-create-starred-note-comment.js
@@ -1,5 +1,10 @@
 'use strict';
-/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('StarredNoteComments', {
@@ -38,9 +43,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
+    options.tableName = "StarredNoteComments";
     await queryInterface.dropTable('StarredNoteComments');
   }
 };
