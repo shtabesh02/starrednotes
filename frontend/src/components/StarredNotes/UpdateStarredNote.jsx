049b1bb4792b2abfde241df1399945e5d2a73fcd
shtabesh02 <shtabesh02@gmail.com>
2024-07-06 10:56:35 -0400

    note updates edited

--- frontend/src/components/StarredNotes/UpdateStarredNote.jsx
+++ frontend/src/components/StarredNotes/UpdateStarredNote.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { loadthenote, updatethenote } from "../../store/starrednotes";
 import './UpdateStarredNote.css'
+import ReactQuill from "react-quill";
 
 const UpdateStarredNote = () => {
 
@@ -37,6 +38,29 @@ const UpdateStarredNote = () => {
         .then(()=> navigate(`/starrednotes/${starrednote_id}`))
     }
 
+    const module = {
+        toolbar: [
+          ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+          ['blockquote', 'code-block'],
+          // ['link', 'image', 'video', 'formula'],
+      
+          // [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+          [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
+          [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+          [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+          [{ 'direction': 'rtl' }],                         // text direction
+      
+          [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+          [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      
+          [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+          [{ 'font': [] }],
+          [{ 'align': [] }],
+      
+          ['clean']                                         // remove formatting button
+        ]
+      }
+
   return (
     <div className="updatenotecontainer">
         <h1>Update your note</h1>
@@ -48,10 +72,16 @@ const UpdateStarredNote = () => {
             </div>
             <div>
                 <label htmlFor="note">Note</label>
-                <textarea name="note" id="note" value={content} onChange={e => setContent(e.target.value)}></textarea>
+                {/* <textarea name="note" id="note" value={content} onChange={e => setContent(e.target.value)}></textarea> */}
+                <ReactQuill
+                    modules={module}
+                    theme='snow'
+                    value={content}
+                    onChange={(content) => setContent(content)}
+                  placeholder='Share your note here now, use it later and anytime...' className='ql-editor' style={{ minHeight: '100px' }} />
             </div>
             <div>
-            <button className="sbmtbtn">Submit</button>
+            <button className="sbmtbtn">Update</button>
             </div>
         </form>}
     </div>
